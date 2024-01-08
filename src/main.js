beb0a0fee0a7dd9f1bfb9c718b2520f901c7940d
Adam Stirtan <adamstirtan@gmail.com>
2024-01-07 20:32:20 -0500

    Cleanup

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 
-import './style.css'
+import './assets/style.css'
 
 createApp(App).use(router).mount('#app')
